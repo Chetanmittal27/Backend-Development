f6dd9eee21527ba4b564ac5da9206f1f3e4d3060
Chetan <chetanmittal39@gmail.com>
2026-06-14 21:39:04 +0530

    Router Learning

--- Professional Backend Project/src/app.js
+++ Professional Backend Project/src/app.js
@@ -22,4 +22,16 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+
+
+// route declaration
+app.use("/api/v1/users" , userRouter);        // this will give control to userRouter and goo to user.routes.js file 
+
+// https://localhost:8000/api/v1/users/register
+
 export {app};
